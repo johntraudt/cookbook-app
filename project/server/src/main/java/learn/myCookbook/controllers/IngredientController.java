69def2966c13c3007c5140c0419b1049a6e48f4a
John Traudt <11581155+johntraudt@users.noreply.github.com>
2020-11-10 14:33:35 -0600

    controller updates

--- project/server/src/main/java/learn/myCookbook/controllers/IngredientController.java
+++ project/server/src/main/java/learn/myCookbook/controllers/IngredientController.java
@@ -1,4 +1,61 @@
 package learn.myCookbook.controllers;
 
+import learn.myCookbook.models.Ingredient;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = {"http://localhost:3000"})
+@RequestMapping("/api/ingredient")
 public class IngredientController {
+
+    private final IngredientService service;
+
+    public IngredientController(IngredientService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Ingredient> findAll() {
+        return service.findAll();
+    }
+
+    @GetMapping("/{ingredientId}")
+    public Ingredient findById(@PathVariable int ingredientId) {
+        return service.findById(ingredientId);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> add(@RequestBody Ingredient ingredient) {
+        Result<Ingredient> result = service.add(ingredient);
+        if (result.isSuccess()) {
+            return new ResponseEntity<>(result.getPayload(), HttpStatus.CREATED);
+        }
+        return ErrorResponse.build(result);
+    }
+
+    @PutMapping("/{ingredientId}")
+    public ResponseEntity<Object> update(@PathVariable int ingredientId, @RequestBody Ingredient ingredient) {
+        if (ingredientId != ingredient.getIngredientId()) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+
+        Result<Ingredient> result = service.update(ingredient);
+        if (result.isSuccess()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        return ErrorResponse.build(result);
+    }
+
+    @DeleteMapping("/{ingredientId}")
+    public ResponseEntity<Void> deleteById(@PathVariable int ingredientId) {
+        if (service.deleteById(ingredientId)) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 }
--- project/server/src/main/java/learn/myCookbook/controllers/ReviewController.java
+++ project/server/src/main/java/learn/myCookbook/controllers/ReviewController.java
@@ -1,4 +1,61 @@
 package learn.myCookbook.controllers;
 
+import learn.myCookbook.models.Review;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = {"http://localhost:3000"})
+@RequestMapping("/api/review")
 public class ReviewController {
+
+    private final ReviewService service;
+
+    public ReviewController(ReviewService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Review> findAll() {
+        return service.findAll();
+    }
+
+    @GetMapping("/{reviewId}")
+    public Review findById(@PathVariable int reviewId) {
+        return service.findById(reviewId);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> add(@RequestBody Review review) {
+        Result<Review> result = service.add(review);
+        if (result.isSuccess()) {
+            return new ResponseEntity<>(result.getPayload(), HttpStatus.CREATED);
+        }
+        return ErrorResponse.build(result);
+    }
+
+    @PutMapping("/{reviewId}")
+    public ResponseEntity<Object> update(@PathVariable int reviewId, @RequestBody Review review) {
+        if (reviewId != review.getReviewId()) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+
+        Result<Review> result = service.update(review);
+        if (result.isSuccess()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        return ErrorResponse.build(result);
+    }
+
+    @DeleteMapping("/{reviewId}")
+    public ResponseEntity<Void> deleteById(@PathVariable int reviewId) {
+        if (service.deleteById(reviewId)) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 }
