a079325893ec84aa232949addf0adea40865b7f2
Devon Burwell <devpburwell@gmail.com>
2020-11-11 11:58:34 -0600

    add public interface to ingredient

--- project/server/src/main/java/learn/myCookbook/data/IngredientJdbcTemplateRepository.java
+++ project/server/src/main/java/learn/myCookbook/data/IngredientJdbcTemplateRepository.java
@@ -1,7 +1,50 @@
 package learn.myCookbook.data;
 
+import learn.myCookbook.data.mappers.IngredientMapper;
+import learn.myCookbook.models.Ingredient;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public class IngredientJdbcTemplateRepository {
+public class IngredientJdbcTemplateRepository implements IngredientRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public IngredientJdbcTemplateRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<Ingredient> findAll() {
+        final String sql = "select ingredient_id, name" +
+                "from ingredient limit 1000";
+        return jdbcTemplate.query(sql, new IngredientMapper());
+    }
+
+    @Override
+    public Ingredient findById() {
+        return null;
+    }
+
+    @Override
+    public Ingredient findByName() {
+        return null;
+    }
+
+    @Override
+    public Ingredient add(Ingredient ingredient) {
+        return null;
+    }
+
+    @Override
+    public boolean update(Ingredient ingredient) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteById(Ingredient ingredient) {
+        return false;
+    }
 }
--- project/server/src/main/java/learn/myCookbook/data/IngredientRepository.java
+++ project/server/src/main/java/learn/myCookbook/data/IngredientRepository.java
@@ -1,4 +1,19 @@
 package learn.myCookbook.data;
 
+import learn.myCookbook.models.Ingredient;
+
+import java.util.List;
+
 public interface IngredientRepository {
+    List<Ingredient> findAll();
+
+    Ingredient findById();
+
+    Ingredient findByName();
+
+    Ingredient add(Ingredient ingredient);
+
+    boolean update(Ingredient ingredient);
+
+    boolean deleteById(Ingredient ingredient);
 }
