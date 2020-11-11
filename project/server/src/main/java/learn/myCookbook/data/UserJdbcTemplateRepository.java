889a26503ae89219712f4363887034e0658ff368
John Traudt <11581155+johntraudt@users.noreply.github.com>
2020-11-11 11:26:27 -0600

    KnownGoodState for testing

--- project/server/src/main/java/learn/myCookbook/data/UserJdbcTemplateRepository.java
+++ project/server/src/main/java/learn/myCookbook/data/UserJdbcTemplateRepository.java
@@ -3,8 +3,12 @@ package learn.myCookbook.data;
 import learn.myCookbook.data.mappers.UserMapper;
 import learn.myCookbook.models.User;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 
 @Repository
@@ -37,12 +41,38 @@ public class UserJdbcTemplateRepository implements UserRepository {
 
     @Override
     public User add(User user) {
-        return null;
+        final String sql = "insert into `user` (first_name, last_name, email, user_role_id) "
+                + "values (?,?,?,?,?);";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        int rowsAffected = jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, user.getFirstName());
+            ps.setString(2, user.getLastName());
+            ps.setString(3, user.getEmail());
+            ps.setInt(4, user.getUserRoleId());
+            return ps;
+        }, keyHolder);
+        if (rowsAffected <= 0) {
+            return null;
+        }
+        user.setUserId(keyHolder.getKey().intValue());
+        return user;
     }
 
     @Override
     public boolean update(User user) {
-        return false;
+        final String sql = "update user set " +
+                "first_name = ?, " +
+                "last_name = ?, " +
+                "email = ? " +
+                "where user_id = ?;";
+
+        return jdbcTemplate.update(sql,
+                user.getFirstName(),
+                user.getLastName(),
+                user.getEmail(),
+                user.getUserId()) > 0;
     }
 
     @Override
--- project/server/src/test/java/learn/myCookbook/data/UserJdbcTemplateRepositoryTest.java
+++ project/server/src/test/java/learn/myCookbook/data/UserJdbcTemplateRepositoryTest.java
@@ -1,7 +1,34 @@
 package learn.myCookbook.data;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class UserJdbcTemplateRepositoryTest {
+
+    @Autowired
+    UserJdbcTemplateRepository repository;
+
+    @Autowired
+    KnownGoodState knownGoodState;
+
+    @BeforeEach
+    void setup() {
+        knownGoodState.set();
+    }
+
+    @Test
+    void shouldFindAll() {
+
+    }
+
+    @Test
+    void shouldFindJohn() {
+        
+    }
 
 }
