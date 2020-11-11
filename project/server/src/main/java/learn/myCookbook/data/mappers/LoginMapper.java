0e2dd963f5902542b287de41d30a1143f2ae4ad2
John Traudt <11581155+johntraudt@users.noreply.github.com>
2020-11-11 14:11:05 -0600

    User updates

---
+++ project/server/src/main/java/learn/myCookbook/data/mappers/LoginMapper.java
@@ -0,0 +1,18 @@
+package learn.myCookbook.data.mappers;
+
+import learn.myCookbook.models.Login;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class LoginMapper implements RowMapper<Login> {
+
+    @Override
+    public Login mapRow(ResultSet resultSet, int i) throws SQLException {
+        Login login = new Login();
+        login.setUserName(resultSet.getString("user_name"));
+        login.setPasswordHash(resultSet.getString("password_hash"));
+        return login;
+    }
+}
