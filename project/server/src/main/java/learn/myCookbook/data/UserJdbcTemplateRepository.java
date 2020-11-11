da0e23e3f98b4414f930a5c15a5988c1edb7828e
John Traudt <11581155+johntraudt@users.noreply.github.com>
2020-11-11 10:39:19 -0600

    test directories

--- project/server/src/main/java/learn/myCookbook/data/UserJdbcTemplateRepository.java
+++ project/server/src/main/java/learn/myCookbook/data/UserJdbcTemplateRepository.java
@@ -1,20 +1,38 @@
 package learn.myCookbook.data;
 
+import learn.myCookbook.data.mappers.UserMapper;
 import learn.myCookbook.models.User;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public class UserJdbcTemplateRepository implements UserRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserJdbcTemplateRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public List<User> findAll() {
-        return null;
+        final String sql = "select user_id, first_name, last_name, email "
+                + "from user limit 1000";
+        return jdbcTemplate.query(sql, new UserMapper());
     }
 
     @Override
     public User findById(int userId) {
-        return null;
+        final String sql = "select user_id, first_name, last_name, email "
+                + "from user limit 1000 "
+                + "where user_id = ?;";
+
+        User user = jdbcTemplate.query(sql, new UserMapper(), userId).stream()
+                .findFirst().orElse(null);
+
+        return user;
     }
 
     @Override
--- project/server/src/main/java/learn/myCookbook/domain/UserService.java
+++ project/server/src/main/java/learn/myCookbook/domain/UserService.java
@@ -1,7 +1,23 @@
 package learn.myCookbook.domain;
 
+import learn.myCookbook.data.UserRepository;
+import learn.myCookbook.models.User;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
 
 @Service
 public class UserService {
+
+    private final UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+
 }
