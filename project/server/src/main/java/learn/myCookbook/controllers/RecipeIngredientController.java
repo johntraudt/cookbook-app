ee0b09253b80e919cfc2734a047b98048579528a
Devon Burwell <devpburwell@gmail.com>
2020-11-12 19:21:17 -0600

    quantity

--- project/server/src/main/java/learn/myCookbook/controllers/RecipeIngredientController.java
+++ project/server/src/main/java/learn/myCookbook/controllers/RecipeIngredientController.java
@@ -1,6 +1,7 @@
 package learn.myCookbook.controllers;
 
 import learn.myCookbook.domain.RecipeIngredientService;
+import learn.myCookbook.models.Recipe;
 import learn.myCookbook.models.RecipeIngredient;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,5 +25,15 @@ public class RecipeIngredientController {
     @GetMapping("/{recipeIngredientId}")
     public RecipeIngredient findById(@PathVariable int recipeIngredientId) {
         return service.findById(recipeIngredientId);
+    }
+
+    @GetMapping("/recipe/{recipeId}")
+    public List<RecipeIngredient> findByRecipeId(@PathVariable int recipeId) {
+        return service.findByRecipeId(recipeId);
+    }
+
+    @GetMapping("/recipe/{recipeId}/{index}")
+    public RecipeIngredient findByRecipeIdAndIndex(@PathVariable int recipeId, @PathVariable int index) {
+        return service.findByRecipeIdAndIndex(recipeId, index);
     }
 }
