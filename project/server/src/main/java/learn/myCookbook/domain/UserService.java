6e7f00e78fba3d4b738bd819462aa624486e1fa3
John Traudt <11581155+johntraudt@users.noreply.github.com>
2020-11-13 10:24:03 -0600

    user service

--- project/server/src/main/java/learn/myCookbook/domain/UserService.java
+++ project/server/src/main/java/learn/myCookbook/domain/UserService.java
@@ -23,9 +23,63 @@ public class UserService {
         return repository.findById(userId);
     }
 
-//    public Result<User> add(User user) {
-//        Result<User>
-//    }
+    public Result<User> add(User user) {
+        Result<User> result = validate(user);
+        if (!result.isSuccess()) {
+            return result;
+        }
+
+        if (user.getUserId() != 0) {
+            result.addMessage("userId cannot be set for `add` operation.", ResultType.INVALID);
+            return result;
+        }
+
+        user = repository.add(user);
+        result.setPayload(user);
+        return result;
+    }
+
+    public Result<User> update(User user) {
+        Result<User> result = validate(user);
+        if (!result.isSuccess()) {
+            return result;
+        }
+
+        if (user.getUserId() <= 0) {
+            result.addMessage("userId must be set for `update` operation.", ResultType.INVALID);
+            return result;
+        }
+
+        if (!repository.update(user)) {
+            String message =  String.format("userId: %s, not found.", user.getUserId());
+            result.addMessage(message, ResultType.INVALID);
+        }
+
+        return result;
+    }
+
+    private Result<User> validate(User user) {
+        // TODO: userRoleId
+        Result<User> result = new Result<>();
+        if (user == null) {
+            result.addMessage("user cannot be null.", ResultType.INVALID);
+            return result;
+        }
+
+        if (Validations.isNullOrBlank(user.getFirstName())) {
+            result.addMessage("firstName is required.", ResultType.INVALID);
+        }
+
+        if (Validations.isNullOrBlank(user.getLastName())) {
+            result.addMessage("lastName is required.", ResultType.INVALID);
+        }
+
+        if (Validations.isNullOrBlank(user.getEmail())) {
+            result.addMessage("email is required.", ResultType.INVALID);
+        }
+
+        return result;
+    }
 
 
 }
