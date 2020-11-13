077816429d31af635181f83e67f8bcba47841fb1
NoahMM <noahmitchelson@gmail.com>
2020-11-12 20:03:23 -0600

    Added responsive rendering to Recipe Page and routing

--- project/client/src/pages/Categories.js
+++ project/client/src/pages/Categories.js
@@ -1,63 +1,30 @@
 import React, {useState, useEffect} from 'react';
 import CicleCard from '../page-elements/CircleCard'
 
-function Categories() {
+export default function Categories() {
     return (
         <div className="container full-body">
             <h1 className="text-center m-4">Categories</h1>
-            <div className="card-grid">
-                <div className="row mt-4 mb-4">
-                    <div className="col-4">
-                        <CicleCard />
-                    </div>
-                    <div className="col-4">
-                        <CicleCard />
-                    </div>
-                    <div className="col-4">
-                        <CicleCard />
-                    </div>
+            <div className="d-flex flex-wrap justify-content-center mt-5 mb-5">
+                <div className="col-4">
+                    <CicleCard />
                 </div>
-                <div className="row mt-4 mb-4">
-                    <div className="col-4">
-                        <CicleCard />
-                    </div>
-                    <div className="col-4">
-                        <CicleCard />
-                    </div>
-                    <div className="col-4">
-                        <CicleCard />
-                    </div>
+                <div className="col-4">
+                    <CicleCard />
                 </div>
-                <div className="row mt-4 mb-4">
-                    <div className="col-4">
-                        <CicleCard />
-                    </div>
-                    <div className="col-4">
-                        <CicleCard />
-                    </div>
-                    <div className="col-4">
-                        <CicleCard />
-                    </div>
+                <div className="col-4">
+                    <CicleCard />
+                </div>
+                <div className="col-4">
+                    <CicleCard />
+                </div>
+                <div className="col-4">
+                    <CicleCard />
+                </div>
+                <div className="col-4">
+                    <CicleCard />
                 </div>
             </div>
-            
-        </div>
-
-        
+        </div>    
     );
 }
-  
-{/* <div className="card-grid">
-<div className="row mt-4 mb-4">
-    <div className="col-4"><SquareCard/></div>
-    <div className="col-4"><SquareCard/></div>
-    <div className="col-4"><SquareCard/></div>
-</div>
-<div className="row mt-4 mb-4">
-    <div className="col-4"><SquareCard/></div>
-    <div className="col-4"><SquareCard/></div>
-    <div className="col-4"><SquareCard/></div>
-</div>
-</div> */}
-
-export default Categories;
