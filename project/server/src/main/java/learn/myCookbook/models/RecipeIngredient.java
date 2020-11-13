62b52bb4377c4639c26e79023975bd5dea2d2da2
Devon Burwell <devpburwell@gmail.com>
2020-11-12 19:46:19 -0600

    categories have links in the database

---
+++ project/server/src/main/java/learn/myCookbook/models/RecipeIngredient.java
@@ -0,0 +1,85 @@
+package learn.myCookbook.models;
+
+public class RecipeIngredient {
+    private int recipeIngredientId;
+    private int recipeId;
+    private int ingredientId;
+    private Recipe recipe;
+    private Ingredient ingredient;
+    private int ingredientListIndex;
+    private String quantity;
+    private int measurementUnitId;
+    private MeasurementUnit measurementUnit;
+
+    public int getRecipeIngredientId() {
+        return recipeIngredientId;
+    }
+
+    public void setRecipeIngredientId(int recipeIngredientId) {
+        this.recipeIngredientId = recipeIngredientId;
+    }
+
+    public int getRecipeId() {
+        return recipeId;
+    }
+
+    public void setRecipeId(int recipeId) {
+        this.recipeId = recipeId;
+    }
+
+    public int getIngredientId() {
+        return ingredientId;
+    }
+
+    public void setIngredientId(int ingredientId) {
+        this.ingredientId = ingredientId;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public Ingredient getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(Ingredient ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public int getIngredientListIndex() {
+        return ingredientListIndex;
+    }
+
+    public void setIngredientListIndex(int ingredientListIndex) {
+        this.ingredientListIndex = ingredientListIndex;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getMeasurementUnitId() {
+        return measurementUnitId;
+    }
+
+    public void setMeasurementUnitId(int measurementUnitId) {
+        this.measurementUnitId = measurementUnitId;
+    }
+
+    public MeasurementUnit getMeasurementUnit() {
+        return measurementUnit;
+    }
+
+    public void setMeasurementUnit(MeasurementUnit measurementUnit) {
+        this.measurementUnit = measurementUnit;
+    }
+}
