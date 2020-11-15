8a0da31ce212bf5208831f37edac76559b9cdba4
John Traudt <11581155+johntraudt@users.noreply.github.com>
2020-11-15 10:23:16 -0600

    Show Me the Money

--- project/client/src/page-elements/Header.js
+++ project/client/src/page-elements/Header.js
@@ -14,7 +14,12 @@ function Header() {
                 <div className="row p-1">
                     <div className={location.pathname !== '/' ? "row mr-auto ml-3": "row mr-auto ml-3"} >
                         <Link to='/' >
-                            <h1>BUILD A COOKBOOK</h1>
+                            <div className="float-left">
+                                <img height="20px" width="20px" src="dino.jpg" alt="dino logo"></img>
+                            </div>
+                            <div className="float-right">
+                                <h1>BUILD A COOKBOOK</h1>
+                            </div>
                         </Link>
                     </div>
                     
@@ -29,7 +34,7 @@ function Header() {
                         
                         <div className="p-2">
                             <Link to='/login'>
-                                <div className="right btn-secondary p-2 pr-4 pl-4">Login</div>
+                                <div className="right btn btn-outline-secondary p-2 pr-4 pl-4">LOG IN</div>
                             </Link>
                         </div>
                     </div>
@@ -44,7 +49,7 @@ function Header() {
                         <Link className="dark" to='/notfound'>
                             <div>Recipe Of The Day</div>
                         </Link>
-                        <Link className="dark" to='/random'>
+                        <Link className="dark"  to='/recipe/random' onClick={() => window.location.reload()}>
                             <div>Show Me The Money</div>
                         </Link>
                     </div>
