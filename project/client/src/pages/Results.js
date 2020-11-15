caf424f64f7911bae1d1b49f07d6e49252f5d4a3
NoahMM <noahmitchelson@gmail.com>
2020-11-14 21:36:41 -0600

    updated search feature

--- project/client/src/pages/Results.js
+++ project/client/src/pages/Results.js
@@ -10,7 +10,7 @@ export default function Results() {
     const searchTerm = history.location.pathname.replace('/results/','');
 
     const featuredRecipes = () => {
-        fetch('http://localhost:8080/api/recipe') 
+        fetch(`http://localhost:8080/api/recipe/search/${searchTerm}`) 
             .then(response => response.json())
             .then((data) => {
                 setRecipes(data);
