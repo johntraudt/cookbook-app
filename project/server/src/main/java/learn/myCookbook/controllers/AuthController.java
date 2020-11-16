3864916e6cd133204a8b4c822128704cf783c0d2
John Traudt <11581155+johntraudt@users.noreply.github.com>
2020-11-16 17:26:38 -0600

    branching

--- project/server/src/main/java/learn/myCookbook/controllers/AuthController.java
+++ project/server/src/main/java/learn/myCookbook/controllers/AuthController.java
@@ -1,6 +1,10 @@
 package learn.myCookbook.controllers;
 
+import learn.myCookbook.domain.AppUserService;
+import learn.myCookbook.models.AppUser;
 import learn.myCookbook.security.JwtConverter;
+import learn.myCookbook.security.ValidationErrorResult;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.ValidationException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,10 +26,12 @@ public class AuthController {
     // that processes an Authentication request.
     private final AuthenticationManager authenticationManager;
     private final JwtConverter converter;
+    private final AppUserService service;
 
-    public AuthController(AuthenticationManager authenticationManager, JwtConverter converter) {
+    public AuthController(AuthenticationManager authenticationManager, JwtConverter converter, AppUserService service) {
         this.authenticationManager = authenticationManager;
         this.converter = converter;
+        this.service = service;
     }
 
 
@@ -60,5 +67,26 @@ public class AuthController {
         }
 
         return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+    }
+
+    @PostMapping("/create_account")
+    public ResponseEntity<?> createAccount(@RequestBody AppUser appUser) {
+        try {
+//            appUser.getRoles().add("USER");
+            service.add(appUser);
+        } catch (ValidationException ex) {
+            ValidationErrorResult validationErrorResult = new ValidationErrorResult();
+            validationErrorResult.addMessage(ex.getMessage());
+            return new ResponseEntity<>(validationErrorResult, HttpStatus.BAD_REQUEST);
+        } catch (DuplicateKeyException ex) {
+            ValidationErrorResult validationErrorResult = new ValidationErrorResult();
+            validationErrorResult.addMessage("The provided username already exists");
+            return new ResponseEntity<>(validationErrorResult, HttpStatus.BAD_REQUEST);
+        }
+
+        HashMap<String, String> map = new HashMap<>();
+        map.put("appUserId", String.valueOf(appUser.getUserId()));
+
+        return new ResponseEntity<>(map, HttpStatus.CREATED);
     }
 }
