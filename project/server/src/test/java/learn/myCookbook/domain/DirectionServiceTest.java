76c56c7f519b54ab028538581fcc33a4c62d29ec
John Traudt <11581155+johntraudt@users.noreply.github.com>
2020-11-17 16:53:59 -0600

    direction service tests initial

--- project/server/src/test/java/learn/myCookbook/domain/DirectionServiceTest.java
+++ project/server/src/test/java/learn/myCookbook/domain/DirectionServiceTest.java
@@ -1,7 +1,21 @@
 package learn.myCookbook.domain;
 
+import learn.myCookbook.data.DirectionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class DirectionServiceTest {
+
+    @Autowired
+    DirectionService service;
+
+    @MockBean
+    DirectionRepository repository;
+
+    
 
 }
