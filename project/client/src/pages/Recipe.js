537aaa559d65c452a030b48ad815d939d971db2c
Devon Burwell <devpburwell@gmail.com>
2020-11-17 14:10:33 -0600

    pulling

--- project/client/src/pages/Recipe.js
+++ project/client/src/pages/Recipe.js
@@ -61,12 +61,18 @@ export default function Recipe() {
     useEffect(() => {
         const getRecipe = () => {
             fetch(`http://localhost:8080/api${location.pathname}`)
-                .then
-                .then(response => response.json())
-                .then((data) => {
-                    setRecipe(data);
-                    console.log(data);
-                });
+                .then((response) => {
+                    if (response.status >= 400) {
+                        history.push("/notfound");
+                    } else{
+                        (response) => response.json()
+                    }
+                })
+                // .then(response => response.json())
+                // .then((data) => {
+                //     setRecipe(data);
+                //     console.log(data);
+                // });
         }
         getRecipe();
     }, [location.pathname]);
