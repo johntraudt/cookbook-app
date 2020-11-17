8980e7b3da4ec5f9a22b57172c0093bcae446ea4
John Traudt <11581155+johntraudt@users.noreply.github.com>
2020-11-17 15:52:22 -0600

    service testing v1

---
+++ project/server/src/test/java/learn/myCookbook/domain/DirectionServiceTest.java
@@ -0,0 +1,7 @@
+package learn.myCookbook.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DirectionServiceTest {
+
+}
--- project/server/src/test/java/learn/myCookbook/domain/RecipeServiceTest.java
+++ project/server/src/test/java/learn/myCookbook/domain/RecipeServiceTest.java
@@ -1,7 +1,53 @@
 package learn.myCookbook.domain;
 
+import learn.myCookbook.data.RecipeRepository;
+import learn.myCookbook.models.Recipe;
+import learn.myCookbook.models.Review;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class RecipeServiceTest {
+
+    @Autowired
+    RecipeService service;
+
+    @MockBean
+    RecipeRepository repository;
+
+    @Test
+    void shouldNotAddNull() {
+        Recipe recipe = null;
+        Result<Recipe> result = service.add(recipe);
+        assertEquals(ResultType.INVALID, result.getType());
+    }
+
+    @Test
+    void shouldNotAddWithId() {
+        Recipe recipe = makeRecipe();
+        recipe.setRecipeId(1);
+        Result<Recipe> result = service.add(recipe);
+        assertEquals(ResultType.INVALID, result.getType());
+    }
+
+    Recipe makeRecipe() {
+        Recipe recipe = new Recipe();
+        recipe.setUserId(1);
+        recipe.setName("Jumbo Test");
+        recipe.setPrepTimeInMinutes(5);
+        recipe.setCookTimeInMinutes(15);
+        recipe.setServings(2);
+        recipe.setDate(LocalDate.of(2020, 11, 9));
+        recipe.setWasUpdated(false);
+        recipe.setFeatured(true);
+        recipe.setImageLink("img.org");
+        return recipe;
+    }
 
 }
