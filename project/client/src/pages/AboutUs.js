117ac5618d09d5addae18dabd18b2e6d41f5e9ba
NoahMM <noahmitchelson@gmail.com>
2020-11-19 12:02:10 -0600

    changed about us statement

--- project/client/src/pages/AboutUs.js
+++ project/client/src/pages/AboutUs.js
@@ -4,9 +4,9 @@ export default function AboutUs() {
     return (
         <div className="container full-body text-center">
             <div className="mt-4">
-                <h3 className="mt-5">
-                    3 Devs who just want to cook.
-                </h3>
+                <h4 className="mt-5 mb-3">
+                    A Recipe Book web app to dicover and share your favorite recipes. 
+                </h4>
                 <div className="col-12">
                     <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQ6CpRYUEVaSaYe9vGunlyzZqy25sd1zuAIfhKAN67bkpNyJJK8rQBeU6yPLQHys-Ye-gF6OeKFz_lV/embed?start=false&loop=false&delayms=60000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
                 </div>
