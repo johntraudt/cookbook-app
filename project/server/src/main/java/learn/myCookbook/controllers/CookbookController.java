82cd2a6be28c01d9d7aad6d082677fae5f0364be
John Traudt <11581155+johntraudt@users.noreply.github.com>
2020-11-19 20:06:54 -0600

    corsConfigurer

--- project/server/src/main/java/learn/myCookbook/controllers/CookbookController.java
+++ project/server/src/main/java/learn/myCookbook/controllers/CookbookController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:3000"})
 @RequestMapping("/api/cookbook")
 public class CookbookController {
     private final CookbookService service;
