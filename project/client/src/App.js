db98e79d129847e50579eeb35ebfd8c035f06258
NoahMM <noahmitchelson@gmail.com>
2020-11-19 20:46:10 -0600

    this is it

--- project/client/src/App.js
+++ project/client/src/App.js
@@ -20,7 +20,7 @@ import './App.css'
 
 import jwt_decode from 'jwt-decode';
 import React, {useState, useEffect} from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 export default function App() {
   const [user, setUser] = useState(null);
--- project/client/src/pages/AboutUs.js
+++ project/client/src/pages/AboutUs.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import dino from '../resources/dino.jpg';
 
 export default function AboutUs() {
     return (
@@ -11,7 +10,6 @@ export default function AboutUs() {
                 <div className="col-12">
                     <iframe height="200px" width='200px' src="https://docs.google.com/presentation/d/e/2PACX-1vQ6CpRYUEVaSaYe9vGunlyzZqy25sd1zuAIfhKAN67bkpNyJJK8rQBeU6yPLQHys-Ye-gF6OeKFz_lV/embed?start=false&loop=false&delayms=60000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
                 </div>
-                <img className="mt-3" height="300px" width="300px" src={dino} alt="dino logo"></img>
             </div>
         </div>
     );
